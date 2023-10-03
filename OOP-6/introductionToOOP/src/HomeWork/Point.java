38eb639a1bd4541ea700003a5d16619a428f57af
Andrey <AndreyP@home>
2023-10-03 18:08:09 +0300

    HomWor

---
+++ OOP-6/introductionToOOP/src/HomeWork/Point.java
@@ -0,0 +1,66 @@
+package HomeWork;
+import java.util.Scanner;
+public class Point {
+
+    private double Ax;
+    public double getAx() {
+        return Ax;
+    }
+    public void setAx(double Ax) {
+        this.Ax = Ax;
+    }
+//============================================================
+    private double Ay;
+    public double getAy() {
+        return Ay;
+    }
+    public void setAy(double Ay) {
+        this.Ay = Ay;
+    }
+ //============================================================
+    private double Bx;
+    public double getBx() {
+        return Bx;
+    }
+    public void setBx(double Bx) {
+        this.Bx = Bx;
+    }
+ //============================================================
+    private double By;
+    public double getBy() {
+        return By;
+    }
+    public void setBy(double By) {
+        this.By = By;
+    }
+
+}
+class distAB{
+    public static void main(String[] args) {
+        String seporate = "==================================================";
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите координаты двух точек A и B :");
+        System.out.print("Введите А по x: ");
+        Point dis = new Point();
+        double Ax = input.nextDouble();
+        dis.setAx(Ax);
+        System.out.print("Введите А по y: ");
+        double Ay = input.nextDouble();
+        dis.setAy(Ay);
+        System.out.println(seporate);
+        System.out.print("Введите B по x: ");
+        double Bx = input.nextDouble();
+        dis.setBx(Bx);
+        System.out.print("Введите B по y: ");
+        double By = input.nextDouble();
+        dis.setBy(By);
+        System.out.println(seporate);
+//==========================================
+        double disX = Ax - Bx;
+        double disB = Ay - By;
+
+        System.out.println("Расстояние между точкой А:%.2f:%.2f и точкой B:%.2f:%.2f = %.2f "
+                .formatted(dis.getAx(),dis.getAy(),dis.getBx(),dis.getBy(),Math.sqrt(disX*disX+disB*disB)));
+        System.out.println(seporate);
+    }
+}
--- OOP-6/introductionToOOP/src/Main.java
+++ OOP-6/introductionToOOP/src/Main.java
@@ -1,17 +1,25 @@
 
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
+Scanner scan = new Scanner(System.in);
         System.out.println("Hello world!");
         
         int a;                  //Объявление переменной 'a' типа 'int'
         Point A = new Point(); // Объявление пременной 'A' типа 'Point'
         A.x = 2;               //создание объекта (экземпляра) 'А' класса 'Point'
         A.y = 3;
+
         System.out.println("X = "+ A.x);
         System.out.println("Y = "+ A.y);
         //Object
+
+        Point B = new Point(); // создаем объект B
+        B.x = 4;
+        B.y = 5;
+        System.out.println("X = "+ B.x);
+        System.out.println("Y = "+ B.y);
+
     }
 }
 
