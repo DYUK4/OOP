f9fae1e62d8bf622f4ef454a858d928b1109082d
Andrey <AndreyP@home>
2023-12-03 14:17:53 +0300

    Singl rensponsibility DONE

--- OOP-6/inheritance_2/src/Main.java
+++ OOP-6/inheritance_2/src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+// import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.FileWriter;
@@ -9,6 +9,9 @@ public class Main
 {
     public static void main(String[] args) throws IOException
     {
+
+
+
         //Inheritance:
         /*Human tommy = new Human("Vercetti", "Tomas", 30);
         System.out.println(tommy);
@@ -58,60 +61,67 @@ public class Main
         }
 
         save(group, "group.txt");*/
-        Human[] group = load("group.txt");
-        System.out.println(group.length);
-        for(int i = 0; i < group.length; i++)
-        {
-            System.out.println(group[i]);
-        }
+
+//        Human[] group = load("group.txt");
+//        System.out.println(group.length);
+//        for(int i = 0; i < group.length; i++)
+//        {
+//            System.out.println(group[i]);
+//        }
+
+File file = new File("group.txt");
+Human[] group = file.load();
+for(int i = 0; i < group.length; i++ ){
+    System.out.println(group[i]);
+}
     }
-    public static void save(Human[] group, String filename)throws IOException
-    {
-        File file = new File(filename);
-        System.out.println(file.getAbsoluteFile());
-        file.delete();
-        file.createNewFile();
-
-        FileWriter writer = new FileWriter(file);
-
-        for(int i = 0 ; i < group.length; i++)
-        {
-            writer.write(((Object)group[i]).getClass().getSimpleName() + ":\t" + group[i].toString() + ";");
-            writer.write('\n');
-        }
-        writer.close(); //Потоки обязательно нужно закрывать
-
-        String command = "C:\\Program Files\\Notepad++\\notepad++ " + filename;
-
-        Process process = Runtime.getRuntime().exec(command);
-    }
-    public static Human[] load(String filename) throws FileNotFoundException {
-        //Human[] group = null;
-        ArrayList<Human> al_group = new ArrayList<>();
-        File file = new File(filename);
-        Scanner scanner = new Scanner(file);    //Создаем и открываем поток чтения из файла
-        while(scanner.hasNextLine())
-        {
-            String buffer = scanner.nextLine();
-            if(buffer.isEmpty())continue;
-            String[] values = buffer.split("[:,;]");
-            values[1] = values[1].replace("\t", "");
-            for(int i = 0; i < values.length; i++)
-                values[i].trim();
-            //System.out.println(buffer);
-//            System.out.print(values.length + ":\t");
-//            for (int i = 0; i < values.length; i++) System.out.print(values[i] + "\t");
-//            System.out.println();
-            Human member = HumanFactory.Create(values[0]);
-            member.init(values);
-            //System.out.println(((Object) member).getClass().getSimpleName());
-            //al_group.add(HumanFactory.Create(values[0]));
-            //al_group.get(al_group.size() - 1).init(values);
-            al_group.add(member);
-        }
-        scanner.close();    //Закрываем поток
-        Human[] group = new Human[al_group.size()];
-        System.out.println(al_group.size());
-        return al_group.toArray(group);
-    }
+//    public static void save(Human[] group, String filename)throws IOException
+//    {
+//        File file = new File(filename);
+//        System.out.println(file.getAbsoluteFile());
+//        file.delete();
+//        file.createNewFile();
+//
+//        FileWriter writer = new FileWriter(file);
+//
+//        for(int i = 0 ; i < group.length; i++)
+//        {
+//            writer.write(((Object)group[i]).getClass().getSimpleName() + ":\t" + group[i].toString() + ";");
+//            writer.write('\n');
+//        }
+//        writer.close(); //Потоки обязательно нужно закрывать
+//
+//        String command = "C:\\Program Files\\Notepad++\\notepad++ " + filename;
+//
+//        Process process = Runtime.getRuntime().exec(command);
+//    }
+//    public static Human[] load(String filename) throws FileNotFoundException {
+//        //Human[] group = null;
+//        ArrayList<Human> al_group = new ArrayList<>();
+//        File file = new File(filename);
+//        Scanner scanner = new Scanner(file);    //Создаем и открываем поток чтения из файла
+//        while(scanner.hasNextLine())
+//        {
+//            String buffer = scanner.nextLine();
+//            if(buffer.isEmpty())continue;
+//            String[] values = buffer.split("[:,;]");
+//            values[1] = values[1].replace("\t", "");
+//            for(int i = 0; i < values.length; i++)
+//                values[i].trim();
+//            //System.out.println(buffer);
+////            System.out.print(values.length + ":\t");
+////            for (int i = 0; i < values.length; i++) System.out.print(values[i] + "\t");
+////            System.out.println();
+//            Human member = HumanFactory.Create(values[0]);
+//            member.init(values);
+//            //System.out.println(((Object) member).getClass().getSimpleName());
+//            //al_group.add(HumanFactory.Create(values[0]));
+//            //al_group.get(al_group.size() - 1).init(values);
+//            al_group.add(member);
+//        }
+//        scanner.close();    //Закрываем поток
+//        Human[] group = new Human[al_group.size()];
+//        System.out.println(al_group.size());
+//        return al_group.toArray(group);
+//    }
 }
